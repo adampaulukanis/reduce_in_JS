e0c1cd60df1566b0730501333d7331bf8b1e7ef7
Adam Paulukanis <adam@Adams-MacBook-Pro.local>
2019-04-10 21:27:03 +0100

    TODO another example

---
+++ example2.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const myFamily = [
+  { firstName: 'Jan', relation: 'father' },
+  { firstName: 'Leonida', relation: 'mother' },
+  { firstName: 'Katarzyna', relation: 'sister' }
+]
+
+// Do something
