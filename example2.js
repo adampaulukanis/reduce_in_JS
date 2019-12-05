ddc0d527b266345415ccc1860e2c12a8d3df458e
Adam Paulukanis <adam.paulukanis@gmail.com>
2019-12-05 19:31:49 +0000

    Another example of using Array#reduce

--- example2.js
+++ example2.js
@@ -1,9 +1,13 @@
 'use strict'
 
-const myFamily = [
-  { firstName: 'Jan', relation: 'father' },
-  { firstName: 'Leonida', relation: 'mother' },
-  { firstName: 'Katarzyna', relation: 'sister' }
-]
+const inputWords = ['Apple', 'Banana', 'Cherry', 'Apple']
 
-// Do something
+// Count how many occurrences of the word the array contains
+function countWords (arr) {
+  return arr.reduce(function (countMap, word) {
+    countMap[word] = ++countMap[word] || 1
+    return countMap
+  }, {}) // second argument to reduce initialises countMap to {}
+}
+
+console.log(countWords(inputWords))
